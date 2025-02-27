ebc96d0345b44130379369c0c1b105bd9553b887
shadia <shadiaalmtiri@gmail.com>
2025-02-27 13:50:34 +0300

    Initial commit - Upload TechJobPortal project

---
+++ Controllers/JobController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobPortal.Models;
+
+namespace TechJobPortal.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            var jobs = new List<JobListing>
+            {
+                new JobListing { Id = 1, Title = "Software Developer", CompanyName = "Apple", Location = "Riyadh", JobType = JobType.FullTime, PostedDate = DateTime.Now.AddDays(-2) },
+                new JobListing { Id = 2, Title = "Data Analyst", CompanyName = "Microsoft", Location = "Jeddah", JobType = JobType.Remote, PostedDate = DateTime.Now.AddDays(-5) },
+                new JobListing { Id = 3, Title = "Web Designer", CompanyName = "Google", Location = "Madinah", JobType = JobType.Contract, PostedDate = DateTime.Now.AddDays(-10) },
+        
+            };
+            return View(jobs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var job = new JobListing { Id = id, Title = "Software Developer", CompanyName = "Apple", Location = "Riyadh", JobType = JobType.FullTime, PostedDate = DateTime.Now.AddDays(-2) };
+            return View(job);
+        }
+    }
+}
---
+++ Models/JobListing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechJobPortal.Models
+{
+    public class JobListing
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        public string Location { get; set; }
+        public JobType JobType { get; set; }
+        public DateTime PostedDate { get; set; }
+    }
+
+    public enum JobType
+    {
+        FullTime,
+        PartTime,
+        Remote,
+        Contract
+    }
+}
